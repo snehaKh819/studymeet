893f66bf9ee964dc7aac3248f9b34a54112e0016
Your Correct GitHub Username <nehagusain541@gmail.com>
2026-06-17 19:16:24 +0530

    User-authentication backend completed

--- user-service/src/app.js
+++ user-service/src/app.js
@@ -1,5 +1,14 @@
 const express=require('express');
-const cookieParser=require('cokkieParser');
+const cookieParser=require('cookie-parser');
 const authRoutes=require('./routes/authRoutes');
 
 const app=express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/auth',authRoutes);
+
+const PORT=process.env.PORT||5000;
+
+app.listen(PORT,()=>{console.log(`Server running on ${PORT}`)});
