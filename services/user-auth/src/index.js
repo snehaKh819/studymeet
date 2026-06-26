5232ce40c722ffd7b4e0d0c6ffa56f95e5d0fa5a
Sneha Khandka <snehakhandka83@gmail.com>
2026-06-26 19:49:37 +0530

    minor tweaks

--- services/user-auth/src/index.js
+++ services/user-auth/src/index.js
@@ -10,8 +10,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true,               
+  origin: ['http://localhost', 'http://localhost:5173'],
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
