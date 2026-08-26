1b384b812bb482671117fdf4502cb98bc3e7438d
Sneha Khandka <snehakhandka83@gmail.com>
2026-08-26 17:36:35 +0530

    basic whiteboard

---
+++ frontend/src/Whiteboard/Whiteboard.jsx
@@ -0,0 +1,12 @@
+import {Tldraw} from 'tldraw';
+import 'tldraw/tldraw.css';
+
+function Whiteboard(){
+    return(
+        <div className="whiteboard">
+            <Tldraw/>
+        </div>
+    );
+}
+
+export default Whiteboard;
